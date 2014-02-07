3443ca1f127669b799e304e105d568c3ae082a61
hkumarmk <hkumarmk@gmail.com>
2014-02-07 07:43:57 +0000

    changed concat::fragment from shared to standard resource
    git-svn-id: https://10.135.121.138/svn/puppet_new_1/trunk/module_source/puppet-ceph@45 7e7508ed-7dfa-4282-9f22-b7e683a64a08

--- manifests/conf/mon.pp
+++ manifests/conf/mon.pp
@@ -5,7 +5,7 @@ define ceph::conf::mon (
   $mon_port,
 ) {
 
-  @@concat::fragment { "ceph-mon-${name}.conf":
+  concat::fragment { "ceph-mon-${name}.conf":
     target  => '/etc/ceph/ceph.conf',
     order   => '50',
     content => template('ceph/ceph.conf-mon.erb'),
